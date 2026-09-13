788392b83e677880fd8ed8fc76f5c52193593d92
hemant kumar <hemantkumar100001@gmail.com>
2026-09-13 17:17:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ USER_DATABASE = {
 
 user_data_store = {
     "12345": {"name": "Player_Alpha", "likes": 50},
-    "987654": {"name": "Player_Beta", "likes": 120}
-     "123456": {"name": "Player_Alpha", "likes": 54},
-    "987654": {"name": "Player_Beta", "likes": 125}
+    "987654": {"name": "Player_Beta", "likes": 120},
+    "123456": {"name": "Player_Alpha", "likes": 54},
+    "987654": {"name": "Player_Beta", "likes": 125},
     "123456": {"name": "Player_Alpha", "likes": 58},
-    "987654": {"name": "Player_Beta", "likes": 129}
+    "987654": {"name": "Player_Beta", "likes": 129},
 }
 
 class UserAuthRequest(BaseModel):
